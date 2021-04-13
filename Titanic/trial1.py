176673c5e22ea386a7510cd1c04b60e780f19816
YomnaJehad <yomnaj96@gmail.com>
2021-04-13 12:48:05 +0200

    First Tabular Data

---
+++ Titanic/trial1.py
@@ -0,0 +1,6 @@
+#FOR THE TITANIC PROBLEM
+# IT IS A BINARY CLASSIFICATION PROBLEM: SURVIVOR=1/ NON-SURVIVOR=0
+
+import pandas as pd 
+df_original_data= pd.read_csv('train.csv')
+df_original_data.head()
